3578173e3d884423a0e34c2425bd5e3d6f06cf45
KS Anandh <anandh.innosify@gmail.com>
2025-12-20 01:12:20 +0530

    vite config file update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -11,17 +11,4 @@ export default defineConfig({
     },
   },
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
->>>>>>> 6539b799f381b516fb7fea82472b275fd167aedb
